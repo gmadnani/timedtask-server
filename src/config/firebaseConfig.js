64e7384d33873be4f62ad039cc59b594b4e260ab
Siddhesh Dighe <siddheshdighe17@gmail.com>
2022-11-30 14:08:22 -0500

    parsing JSON from env variable

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,7 +2,7 @@ import admin from 'firebase-admin';
 //import serviceAccount from "./serviceAccountKey.json" assert {type: 'json'};
 
 admin.initializeApp({
-    credential: admin.credential.cert(process.env.SERVICE_ACCOUNT) 
+    credential: admin.credential.cert(JSON.parse(process.env.SERVICE_ACCOUNT)) 
 })
 
 export default admin
